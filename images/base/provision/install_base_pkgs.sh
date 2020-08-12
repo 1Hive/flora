f54693cb84e27d0412b06cb158c703863042cc96
Oliver Nordbjerg <hi@notbjerg.me>
2020-08-12 11:02:28 +0200

    Silence all interactive prompts during upgrade procedure

--- images/base/provision/install_base_pkgs.sh
+++ images/base/provision/install_base_pkgs.sh
@@ -1,10 +1,16 @@
 # Configuration
 export GO_VER="1.15"
-export DEBIAN_FRONTEND=noninteractive
+
+# Disable all interactive prompts
+export UCF_FORCE_CONFFOLD=1
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 # Upgrade system packages
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold" \
+  upgrade -y -q
 
 # Install build-essential
 sudo apt-get -y install build-essential git wget
